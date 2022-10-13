abf05c568dde726fb6f70e226f1da21b98647a94
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2022-10-13 18:25:32 -0300

    Created location api configurations

---
+++ api/location/location/urls.py
@@ -0,0 +1,47 @@
+"""location URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from core import views
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+                                   SpectacularSwaggerView)
+from location import settings
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register("location", views.LocationViewSet)
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        # swagger patterns
+        path("api/docs/schema/", SpectacularAPIView.as_view(), name="schema"),
+        # Optional UI:
+        path(
+            "api/docs/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/docs/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+        ),
+        
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
